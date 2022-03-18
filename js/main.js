e0d0848c889c9df440abd838dbdde116ed6bc20f
Matteo Anselmo <matteo.anselmo96@gmail.com>
2022-03-18 13:45:14 +0100

    inizializzazione vue todolist

---
+++ js/main.js
@@ -0,0 +1,43 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        todoList: [
+            // {
+            //     text: '',
+            //     done: false  
+            // },
+            // {
+            //     text: '',
+            //     done: false  
+            // },
+            // {
+            //     text: '',
+            //     done: false  
+            // },
+        ],
+        newTodoContent: "",
+    },
+
+    methods: {
+        addTodoElement(todoContent){
+            if(todoContent.trim() != ""){
+                const newtodoElement = {
+                    text: todoContent.trim(),
+                    done: false
+                }
+                this.todoList.push(newtodoElement);
+                this.newTodoContent = "";
+            }else{
+                console.warn("la stringa è vuota IDIOTA!")
+            }
+        },
+        removeTodoElement(todoIndex){
+            if(this.todoList[todoIndex] != undefined){
+                this.todoList.splice(todoIndex, 1);
+            } else {
+                console.warn('Vuoi rimuover un oggeto che non esiste')
+            }
+        }
+
+    }
+})
